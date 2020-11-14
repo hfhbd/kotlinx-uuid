bd80f86b65199fa537d9c82e7c44a45d7400280d
hfhbd <hfhbd@users.noreply.github.com>
2020-11-14 13:16:42 +0100

    Add KDoc and API Dump

--- core/src/jvmMain/kotlin/kotlinx/uuid/Converting.kt
+++ core/src/jvmMain/kotlin/kotlinx/uuid/Converting.kt
@@ -4,5 +4,14 @@
 
 package kotlinx.uuid
 
+/**
+ * Converts this [java.util.UUID][java.util.UUID] value to a [kotlinx.uuid.UUID][UUID] value
+ * by using the default [toString] representation.
+ */
 public fun java.util.UUID.toKotlinUUID(): UUID = UUID(toString())
+
+/**
+ * Converts this [kotlinx.uuid.UUID][UUID] value to a [java.util.UUID][java.util.UUID] value
+ * by using the default [toString] representation.
+ */
 public fun UUID.toJavaUUID(): java.util.UUID = java.util.UUID.fromString(toString())
