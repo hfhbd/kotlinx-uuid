ee1bda2548670c5f88d5ee432b986819e6ce45fb
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-05-04 22:52:17 +0200

    Fix Ktor-server module structure (#9)
    * Fix ktor-server folder structure
    
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- kotlinx-uuid-ktor-server/build.gradle.kts
+++ kotlinx-uuid-ktor-server/build.gradle.kts
@@ -1,6 +1,11 @@
 /*
- * Copyright 2020-2020 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2020-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2021 hfhbd and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
+
+plugins {
+    kotlin("multiplatform")
+}
 
 description = "kotlinx-uuid integration for ktor-server"
 
@@ -26,7 +31,6 @@ kotlin {
         }
         getByName("jvmTest") {
             dependencies {
-                implementation(kotlin("test-junit"))
                 implementation("io.ktor:ktor-server-test-host:$ktorVersion")
             }
         }
