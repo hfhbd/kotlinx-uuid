06be80498d453bdae796e44d6c9e623dbbd6a7ab
hfhbd <hfhbd@users.noreply.github.com>
2021-07-15 15:12:46 +0200

    Add mavenCentral stage publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     kotlin("plugin.serialization") version "1.5.21" apply false
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.6.0"
     `maven-publish`
+    signing
 }
 
 repositories {
@@ -18,23 +19,67 @@ group = "app.softwork"
 
 subprojects {
     apply(plugin = "org.gradle.maven-publish")
+    apply(plugin = "org.gradle.signing")
 
     repositories {
         mavenCentral()
     }
 
     group = "app.softwork"
-    
+
     publishing {
         repositories {
-            maven {
+            maven(url = uri("https://maven.pkg.github.com/hfhbd/kotlinx-uuid")) {
                 name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/hfhbd/kotlinx-uuid")
                 credentials {
                     username = System.getenv("GITHUB_ACTOR")
                     password = System.getenv("GITHUB_TOKEN")
                 }
             }
+            maven(url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")) {
+                name = "OSSRH-Stage"
+                credentials {
+                    username = "hfhbd"
+                    password = System.getProperty("sonartype.password") ?: System.getenv("SONARTYPE_PASSWORD")
+                }
+            }
         }
+
+        publications.create<MavenPublication>("mavenPub") {
+            pom {
+                name.set("app.softwork UUID Library")
+                description.set("A multiplatform Kotlin UUID library, forked from https://github.com/cy6erGn0m/kotlinx-uuid")
+                url.set("https://github.com/hfhbd/kotlinx-uuid")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("hfhbd")
+                        name.set("Philip Wedemann")
+                        email.set("mybztg+mavencentral@icloud.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git://github.com/hfhbd/kotlinx-uuid.git")
+                    developerConnection.set("scm:git://github.com/hfhbd/kotlinx-uuid.git")
+                    url.set("https://github.com/hfhbd/kotlinx-uuid")
+                }
+            }
+        }
+    }
+    signing {
+        System.getenv().forEach { t, u ->
+            println("key $t and value $u")
+        }
+        val key = (System.getProperty("signing.privateKey") ?: System.getenv("SIGNING_PRIVATE_KEY")!!).let {
+            String(java.util.Base64.getDecoder().decode(it)).trim()
+        }
+        val signingPassword = System.getProperty("signing.password") ?: System.getenv("SIGNING_PASSWORD")
+        useInMemoryPgpKeys(key, signingPassword)
+        sign(configurations.archives.get())
     }
 }
