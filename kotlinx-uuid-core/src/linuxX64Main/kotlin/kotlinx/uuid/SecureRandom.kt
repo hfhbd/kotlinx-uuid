d1b53f364f39228e46cd876e748dbfbbcaef18f7
hfhbd <hfhbd@users.noreply.github.com>
2022-06-09 20:48:22 +0200

    Use ByteArray for urandom

--- kotlinx-uuid-core/src/linuxX64Main/kotlin/kotlinx/uuid/SecureRandom.kt
+++ kotlinx-uuid-core/src/linuxX64Main/kotlin/kotlinx/uuid/SecureRandom.kt
@@ -9,15 +9,20 @@ public actual val SecureRandom: Random = DevUrandom()
 private class DevUrandom : Random() {
     override fun nextBits(bitCount: Int): Int {
         require(bitCount > 0)
+        val numberOfBytes = (bitCount + Byte.SIZE_BITS) / Byte.SIZE_BITS
+        val bytes = ByteArray(size = numberOfBytes)
         val urandom = open("/dev/urandom", O_RDONLY)
         require(urandom >= 0)
-        val (status, random) =  memScoped {
-            val result: IntVar = alloc()
-            read(urandom, result.ptr, bitCount.toULong()) to result.value
+        val status = bytes.usePinned {
+            read(urandom, it.addressOf(0), numberOfBytes.toULong())
         }
-        require(status >= 0)
-        val result = random ushr (32 - bitCount)
         close(urandom)
+        require(status >= 0)
+        var result = 0
+        for (byte in bytes) {
+            result = (result or byte.toInt()) shl Byte.SIZE_BITS
+        }
+
         return result
     }
 }
