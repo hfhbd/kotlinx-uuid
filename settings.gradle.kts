8701769c20e08d321e30db09513d46648dca35c2
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-08-25 15:31:34 +0200

    Bump Gradle to 7.5.1 (#143)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,3 @@
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 /*
  * Copyright 2020-2021 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  * Copyright 2021 hfhbd and contributors. Use of this source code is governed by the Apache 2.0 license.
