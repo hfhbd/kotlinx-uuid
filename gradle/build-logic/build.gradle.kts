a1552283d1d8d2a4a417a96a5608c9191e6338f1
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-02-04 19:37:17 +0100

    Refactor build dependencies (#296)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- gradle/build-logic/build.gradle.kts
+++ gradle/build-logic/build.gradle.kts
@@ -3,12 +3,16 @@ plugins {
 }
 
 dependencies {
-    implementation(libs.kotlin.gradlePlugin)
-    implementation(libs.kotlin.serialization)
-    implementation(libs.binary)
-    implementation(libs.publish)
-    implementation(libs.dokka)
-    implementation(libs.licensee)
-    implementation(libs.kover)
-    implementation(libs.detekt)
+    implementation(libs.plugins.kotlin.jvm.toDep())
+    implementation(libs.plugins.kotlin.serialization.toDep())
+    implementation(libs.plugins.binary.toDep())
+    implementation(libs.plugins.publish.toDep())
+    implementation(libs.plugins.dokka.toDep())
+    implementation(libs.plugins.licensee.toDep())
+    implementation(libs.plugins.kover.toDep())
+    implementation(libs.plugins.detekt.toDep())
+}
+
+fun Provider<PluginDependency>.toDep() = map {
+    "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version}"
 }
