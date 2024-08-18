c582912539506a66bb73e21778124c37dc7f0110
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-08-18 23:53:17 +0200

    Add basic UuidSerializer until a new kotlinx-serialization release

--- kotlinx-uuid-sqldelight/src/commonMain/kotlin/app/softwork/uuid/sqldelight/UuidByteArrayAdapter.kt
+++ kotlinx-uuid-sqldelight/src/commonMain/kotlin/app/softwork/uuid/sqldelight/UuidByteArrayAdapter.kt
@@ -3,7 +3,7 @@ package app.softwork.uuid.sqldelight
 import app.cash.sqldelight.*
 import kotlin.uuid.Uuid
 
-public object UuidByteArrayAdapter : ColumnAdapter<Uuid, ByteArray> {
+public data object UuidByteArrayAdapter : ColumnAdapter<Uuid, ByteArray> {
     override fun decode(databaseValue: ByteArray): Uuid = Uuid.fromByteArray(databaseValue)
     override fun encode(value: Uuid): ByteArray = value.toByteArray()
 }
