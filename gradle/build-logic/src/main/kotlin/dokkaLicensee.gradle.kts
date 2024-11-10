44678f8ccac85cc255f76bea3eac7dbc4de8f5e7
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-11-10 08:34:17 +0100

    Switch to mavencentral publishing portal

--- gradle/build-logic/src/main/kotlin/dokkaLicensee.gradle.kts
+++ gradle/build-logic/src/main/kotlin/dokkaLicensee.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("app.cash.licensee")
 }
 
-tasks.named<DokkaTaskPartial>("dokkaHtmlPartial") {
+dokka {
     val module = project.name
     dokkaSourceSets.configureEach {
         reportUndocumented.set(true)
@@ -14,16 +14,22 @@ tasks.named<DokkaTaskPartial>("dokkaHtmlPartial") {
         File("$module/src/$sourceSetName").takeIf { it.exists() }?.let {
             sourceLink {
                 localDirectory.set(file("src/$sourceSetName/kotlin"))
-                remoteUrl.set(uri("https://github.com/hfhbd/kotlinx-uuid/tree/main/$module/src/$sourceSetName/kotlin").toURL())
+                remoteUrl.set(uri("https://github.com/hfhbd/kotlinx-uuid/tree/main/$module/src/$sourceSetName/kotlin"))
                 remoteLineSuffix.set("#L")
             }
         }
-        externalDocumentationLink("https://kotlinlang.org/api/kotlinx.serialization/")
-        externalDocumentationLink(
-            url = "https://kotlinlang.org/api/kotlinx-datetime/",
-            packageListUrl = "https://kotlinlang.org/api/kotlinx-datetime/kotlinx-datetime/package-list",
-        )
-        externalDocumentationLink("https://cashapp.github.io/sqldelight/2.0.2/2.x/")
+        externalDocumentationLinks {
+            register("kotlinx.serialization") {
+                url("https://kotlinlang.org/api/kotlinx.serialization/")
+            }
+            register("kotlinx.datetime") {
+                url("https://kotlinlang.org/api/kotlinx-datetime/")
+                packageListUrl("https://kotlinlang.org/api/kotlinx-datetime/kotlinx-datetime/package-list")
+            }
+            register("sqldelight") {
+                url("https://cashapp.github.io/sqldelight/2.0.2/2.x/")
+            }
+        }
     }
 }
 
