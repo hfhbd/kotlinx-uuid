12bc1073ce27424fce123e5c386d84453bd31460
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-02-11 17:38:52 +0100

    Test builtin android parceler

--- kotlinx-uuid-core/src/androidInstrumentedTest/kotlin/app/softwork/uuid/internal/ParcelableTest.kt
+++ kotlinx-uuid-core/src/androidInstrumentedTest/kotlin/app/softwork/uuid/internal/ParcelableTest.kt
@@ -3,7 +3,6 @@ package app.softwork.uuid.internal
 import android.os.Parcel
 import android.os.Parcelable
 import app.softwork.uuid.SOME_UUID_STRING
-import app.softwork.uuid.UuidParceler
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.TypeParceler
 import kotlinx.parcelize.parcelableCreator
@@ -12,17 +11,6 @@ import kotlin.test.assertEquals
 import kotlin.uuid.Uuid
 
 class ParcelableTest {
-    @Test
-    fun testParcelable() {
-        val parcel = Parcel.obtain()
-        val uuid = Uuid.parse(SOME_UUID_STRING)
-        with(UuidParceler) {
-            uuid.write(parcel, 0)
-        }
-        parcel.setDataPosition(0)
-        assertEquals(uuid, UuidParceler.create(parcel))
-    }
-
     @Test
     fun testBox() {
         val parcel = Parcel.obtain()
@@ -34,6 +22,5 @@ class ParcelableTest {
     }
 
     @Parcelize
-    @TypeParceler<Uuid, UuidParceler>()
     data class Box(val uuid: Uuid) : Parcelable
 }
