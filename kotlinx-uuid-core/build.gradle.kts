5f74f34fa01529d65d3c82715dbd8f062c377391
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-05-12 19:52:13 +0200

    Disable project repos

--- kotlinx-uuid-core/build.gradle.kts
+++ kotlinx-uuid-core/build.gradle.kts
@@ -29,17 +29,3 @@ kotlin {
         }
     }
 }
-
-tasks.compileJava9Java {
-    options.compilerArgumentProviders += object : CommandLineArgumentProvider {
-
-        @InputFiles
-        @PathSensitive(PathSensitivity.RELATIVE)
-        val kotlinClasses = tasks.compileKotlinJvm.flatMap { it.destinationDirectory }
-
-        override fun asArguments(): List<String> = listOf(
-            "--patch-module",
-            "app.softwork.uuid.core=${kotlinClasses.get().asFile.absolutePath}"
-        )
-    }
-}
