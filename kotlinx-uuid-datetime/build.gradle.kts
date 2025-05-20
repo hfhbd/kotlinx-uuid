24d013ab22c924aa3bf724cb8d299308cebd6bca
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2025-05-20 08:14:49 +0000

    Bump Kotlin to 2.2.0-RC

--- kotlinx-uuid-datetime/build.gradle.kts
+++ kotlinx-uuid-datetime/build.gradle.kts
@@ -24,3 +24,17 @@ kotlin{
         }
     }
 }
+
+tasks.compileJava9Java {
+    options.compilerArgumentProviders += object : CommandLineArgumentProvider {
+
+        @InputFiles
+        @PathSensitive(PathSensitivity.RELATIVE)
+        val kotlinClasses = tasks.compileKotlinJvm.flatMap { it.destinationDirectory }
+
+        override fun asArguments(): List<String> = listOf(
+            "--patch-module",
+            "app.softwork.uuid.datetime=${kotlinClasses.get().asFile.absolutePath}"
+        )
+    }
+}
