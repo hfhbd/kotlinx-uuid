a06547dec373a9343e931d5aa480352259024f1d
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-06-28 16:08:33 +0200

    Remove outdated datetime artifact

--- kotlinx-uuid-datetime/src/jvmJava9/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module app.softwork.uuid.datetime {
-    exports app.softwork.uuid.datetime;
-
-    requires transitive kotlin.stdlib;
-
-    requires transitive app.softwork.uuid.core;
-    requires transitive kotlinx.datetime;
-}
